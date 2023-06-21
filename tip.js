286d6d551c471f5eb6bd814a9faf3c879940ff8a
Justice Okoli <126474253+Captbull@users.noreply.github.com>
2023-06-21 11:28:22 +0100

    Update tip.js

--- tip.js
+++ tip.js
@@ -1,62 +1,62 @@
  // Defining and assigning required variables
 
-const amount = document.querySelector("#amount");
-const guests = document.querySelector("#guests");
-const willingGuest = document.querySelector("#yes");
-const quality = document.querySelector("#quality");
-const tipAmount = document.querySelector("#result");
-const reset = document.querySelector('#reset')
-
-// the function that will be used to calculate the tip is defined
-
-const calculate = () => {
-    // If the guest(s) are wiling to share the tip bill, this line of code will run
-
-    if (willingGuest.value === '1') {
-        const tip = ((amount.value * quality.value) / (guests.value)).toFixed(2)
-        amount.value = "";
-        guests.value = "";
-        quality.value = "";
-            console.log(tip);
-        // This is to keep the code from displaying technical jargon to the client
-
-        if (tip === "NaN") {
-            tipAmount.textContent ='Tip ₦0 each'
-
-        }
-        else{
-            tipAmount.textContent ='Tip ₦' + tip + 'each'
-        }
-    }
-    // This syntax will run if the guest(s) is not willing to share the bill
-    else {
-        tip = ((amount.value * quality.value)).toFixed(2)
-        amount.value = "";
-        guests.value = "";
-        quality.value = "";
-        
-        // This is to keep the code from displaying technical jargon to user
-
-        if (tip === "NaN") {
-            tipAmount.textContent = 'Please enter valid tip amount'
-        }
-        else{
-            tipAmount.textContent = 'Tip ₦' + tip 
-        }
-    }
-
-    console.log(tipAmount);
-}
-
-reset.onclick = () => {
-    tipAmount.textContent = " "
-}
-
-
-// The naira symbol was copied from https://www.cyberdefinitions.com/symbols/currency-symbols/naira-sign.html
-
-
-
+ const amount = document.querySelector("#amount");
+ const guests = document.querySelector("#guests");
+ const willingGuest = document.querySelector("#yes");
+ const quality = document.querySelector("#quality");
+ const tipAmount = document.querySelector("#result");
+ const reset = document.querySelector('#reset')
+ 
+ // the function that will be used to calculate the tip is defined
+ 
+ const calculate = () => {
+     // If the guest(s) are wiling to share the tip bill, this line of code will run
+ 
+     if (willingGuest.value === '1') {
+         const tip = ((amount.value * quality.value) / (guests.value)).toFixed(2)
+         amount.value = "";
+         guests.value = "";
+         quality.value = "";
+            //  console.log(tip);
+         // This is to keep the code from displaying technical jargon to the client
+ 
+         if (tip === "NaN") {
+             tipAmount.textContent ='Tip ₦0 each'
+ 
+         }
+         else{
+             tipAmount.textContent ='Tip ₦' + tip + 'each'
+         }
+     }
+     // This syntax will run if the guest(s) is not willing to share the bill
+     else {
+         tip = ((amount.value * quality.value)).toFixed(2)
+         amount.value = "";
+         guests.value = "";
+         quality.value = "";
+         
+         // This is to keep the code from displaying technical jargon to user
+ 
+         if (tip === "NaN") {
+             tipAmount.textContent = 'Tip ₦0'
+         }
+         else{
+             tipAmount.textContent = 'Tip ₦' + tip 
+         }
+     }
+ 
+    //  console.log(tipAmount);
+ }
+ 
+ reset.onclick = () => {
+     tipAmount.textContent = " "
+ }
+ console.log('hello');
+ 
+ // The naira symbol was copied from https://www.cyberdefinitions.com/symbols/currency-symbols/naira-sign.html
+ 
+ 
+ 
 
 
 
